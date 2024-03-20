c61e89553175714d6eed14b8c75f8b57af902392
Th-Sagar <sagarthapa98122@gmail.com>
2024-03-20 08:23:52 +0545

    feat:product

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Image from "next/image";
 
-const page = () => {
+const Page = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("https://fakestoreapi.com/products");
+      setData(result.data);
+      console.log(result.data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <>
-
     <section>
-      
-      hii
+      {data.map((item) => (
+        <div key={item.id}>
+          <h1>{item.title}</h1>
+          <p>{item.price}</p>
+          <Image src={item.image} alt={item.title} width={200} height={200} />
+        </div>
+      ))}
     </section>
-
-
- 
-      
-    </>
   );
 };
 
-export default page;
+export default Page;
