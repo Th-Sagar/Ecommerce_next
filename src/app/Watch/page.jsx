5de2a519d9c366e8c30b5f39ff2e5052294f13e0
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-05 07:23:59 +0545

    feat: validate

--- src/app/Watch/page.jsx
+++ src/app/Watch/page.jsx
@@ -1,30 +1,20 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Watch = () => {
   return (
     <section>
-        <main>
+      <main>
+        <div>
+          <Image src={"dafd"} alt="watch" height={250} width={250} />
+        </div>
+      </main>
 
-            <div>
-<Image src={"dafd"} alt='watch' height={250} width={250}/>
+      <main></main>
 
-            </div>
-
-        </main>
-
-        <main>
-
-        </main>
-
-
-        <main>
-
-        </main>
-
-
+      <main></main>
     </section>
-  )
-}
+  );
+};
 
-export default Watch
+export default Watch;
