3ac991296a2765535719521433eafe28d16645d5
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-07 07:23:32 +0545

    feat: validate

--- src/app/Watch/page.jsx
+++ src/app/Watch/page.jsx
@@ -10,7 +10,11 @@ const Watch = () => {
         </div>
       </main>
 
-      <main></main>
+      <main>
+        <h1>
+          
+        </h1>
+      </main>
 
       <main></main>
     </section>
