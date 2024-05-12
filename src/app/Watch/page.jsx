bd53eaac603c1bceb2c690c84cd8c2856a604879
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-12 18:23:24 +0545

    feat:watch

--- src/app/Watch/page.jsx
+++ src/app/Watch/page.jsx
@@ -1,19 +1,75 @@
 import Image from "next/image";
 import React from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const Watch = () => {
   return (
-    <section>
-      <main>
+    <section className=" mt-10 flex">
+      <main className=" w-1/3">
         <div>
-          <Image src={"dafd"} alt="watch" height={250} width={250} />
+          <Image
+            src={"/items/items1.jpg"}
+            alt="watch"
+            height={250}
+            width={250}
+          />
+        </div>
+        <hr className=" my-5 w-full" />
+        <div className=" gap-10 ml-5 mb-4 flex justify-center items-center">
+          <FaChevronLeft
+            size={20}
+            className=" mr-5 flex justify-center items-center"
+          />
+          <Image
+            src={"/watch/watch1.webp"}
+            className=" border hover:border hover:border-orange-500 p-3"
+            alt="watch"
+            height={50}
+            width={50}
+          />
+          <Image
+            src={"/watch/watch2.webp"}
+            className=" border hover:border hover:border-orange-500 p-3"
+            alt="watch"
+            height={50}
+            width={50}
+          />
+          <Image
+            src={"/watch/watch3.webp"}
+            className=" border hover:border hover:border-orange-500 p-3"
+            alt="watch"
+            height={50}
+            width={50}
+          />
+          <Image
+            src={"/watch/watch4.webp"}
+            className=" border hover:border hover:border-orange-500 p-3"
+            alt="watch"
+            height={50}
+            width={50}
+          />
+          <FaChevronRight
+            size={20}
+            className="ml-5 flex justify-center items-center "
+          />
         </div>
       </main>
 
       <main>
-        <h1>
-          
+        <h1 className=" font-bold text-lg mb-4">
+          T800 Ultra Smartwatch Series 8 1.99 Inch
         </h1>
+        <p className=" mb-4">
+          rating 4.5 <span className=" text-blue-700 border-r border-r-black pr-2 mr-2"> 3 Ratings</span>{" "}
+          <span className=" text-blue-700"> 3 Answered Questions</span>
+        </p>
+        <p>
+          Brand <span className=" text-blue-700 border-r border-r-black pr-2 mr-2"> No Brand </span>{" "}
+          <span className=" text-blue-700">
+            {" "}
+            More Wearable Technology from No Brand
+          </span>
+        </p>
       </main>
 
       <main></main>
